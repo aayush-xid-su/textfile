ccff87efad7055a754f54707f16ae883ef840a96
aayush_xid_su <ayushxidsu@gmail.com>
2024-10-28 21:49:04 +0530

    add snippet

--- txtfile.py
+++ txtfile.py
@@ -1,3 +1,27 @@
+# Color snippets
+black = "\033[0;30m"
+red = "\033[0;31m"
+bred = "\033[1;31m"
+green = "\033[0;32m"
+bgreen = "\033[1;32m"
+yellow = "\033[0;33m"
+byellow = "\033[1;33m"
+blue = "\033[0;34m"
+bblue = "\033[1;34m"
+purple = "\033[0;35m"
+bpurple = "\033[1;35m"
+cyan = "\033[0;36m"
+bcyan = "\033[1;36m"
+white = "\033[0;37m"
+nc = "\033[00m"
+
+# Regular Snippets
+ask = f"{green}[{white}?{green}] {yellow}"
+success = f"{yellow}[{white}√{yellow}] {green}"
+error = f"{blue}[{white}!{blue}] {red}"
+info = f"{yellow}[{white}+{yellow}] {cyan}"
+info2 = f"{green}[{white}•{green}] {purple}"
+
 banner = f"""                                                                                                              
 {red}      nnnnz   nnnt  nnn   onnnnnp        znlllo     nnnnnnnnnnr  nnn     znlllq                aaaasyush  xidsuuuu       
 {red}       h000ss000q  u000v  b00000000d   j000000000l z0000000000f  000z  g000000000                aaaayush xiddddd         
